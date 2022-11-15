ac0969c93feade10676439322c67173017e4337d
SpiderPigzz <Ricky.winarko@gmail.com>
2022-11-16 00:34:44 +0800

    Added animations

--- src/script.js
+++ src/script.js
@@ -4,6 +4,26 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import './style.css'
 import './index.html'
+import anime from 'animejs/lib/anime.es.js';
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.8em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
 /**
  * Base
